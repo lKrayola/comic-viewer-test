4893b3334389e5065c11a9eaae42b39d3f884231
lKrayola <kantkendall@gmail.com>
2023-03-31 19:16:45 -0500

    ✨ feat: implement pocketbase library db comic chapter call

---
+++ src/pages/api/chapters/view.ts
@@ -0,0 +1,37 @@
+// next
+import { NextApiRequest, NextApiResponse } from "next";
+import PocketBase from "pocketbase";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const chapterId = req.query.id?.toString();
+
+  const pb = new PocketBase(process.env.POCKETBASE_API_URL);
+
+  const authData = await pb.admins.authWithPassword(
+    `${process.env.POCKETBASE_ADMIN_EMAIL}`,
+    `${process.env.POCKETBASE_ADMIN_PASSWORD}`
+  );
+  // after the above you can also access the auth data from the authStore
+  // console.log(pb.authStore.isValid);
+  // console.log(pb.authStore.token);
+  // console.log(pb.authStore.model.id);
+
+  try {
+    const record = await pb.collection("chapter").getOne(chapterId || "", {
+      expand: "pages",
+    });
+
+    // console.warn(record);
+    // console.warn(record.expand.pages);
+    // debugger;
+
+    return res.status(200).json({ record });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
--- src/pages/comic-viewer.tsx
+++ src/pages/comic-viewer.tsx
@@ -1,11 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+
 // import react dynamic function
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-// Disable server-side rendering for the ComicViewer component by using the dynamic component from Next.js. 
+// Disable server-side rendering for the ComicViewer component by using the dynamic component from Next.js.
 // This will ensure that the component is only rendered on the client-side and not on the server.
 const DynamicComicViewer = dynamic(() => import("react-comic-viewer"), {
   ssr: false,
@@ -14,18 +14,27 @@ const DynamicComicViewer = dynamic(() => import("react-comic-viewer"), {
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [chatperPages, setChapterPages] = useState([]);
+
+  const fetchData = async () => {
+    const chapterId = new URLSearchParams({ id: "juc245vegaemm49" });
+
+    const response = await axios.get(
+      `/api/chapters/view?${chapterId.toString()}`
+    );
+
+    const pagesSourceList: [] = response.data.record.expand.pages.map(
+      (page: any) => page.source
+    );
+    setChapterPages(pagesSourceList);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     // use the new dynamic comic viewer instead of the default.
-    <DynamicComicViewer
-      direction="ltr"
-      pages={[
-        "https://picsum.photos/id/9/600/600",
-        "https://picsum.photos/id/20/600/600",
-        "https://picsum.photos/id/26/600/600",
-        "https://picsum.photos/id/796/600/600",
-        "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
-        "https://picsum.photos/id/882/600/600",
-      ]}
-    />
+    <DynamicComicViewer direction="rtl" pages={chatperPages} />
   );
 }
