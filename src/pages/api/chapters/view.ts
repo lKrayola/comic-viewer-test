8eb67546857ea4e23efc31ba14151fbffc5ce569
lKrayola <kantkendall@gmail.com>
2023-03-31 20:54:56 -0500

    🔧 chore: apply prettier & modify printWidth

--- src/pages/api/chapters/view.ts
+++ src/pages/api/chapters/view.ts
@@ -1,14 +1,14 @@
 // next
-import { NextApiRequest, NextApiResponse } from "next";
-import PocketBase from "pocketbase";
+import { NextApiRequest, NextApiResponse } from 'next'
+import PocketBase from 'pocketbase'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const chapterId = req.query.id?.toString();
+  const chapterId = req.query.id?.toString()
 
-  const pb = new PocketBase(process.env.POCKETBASE_API_URL);
+  const pb = new PocketBase(process.env.POCKETBASE_API_URL)
 
   // const authData = await pb.admins.authWithPassword(
   //   `${process.env.POCKETBASE_ADMIN_EMAIL}`,
@@ -20,20 +20,20 @@ export default async function handler(
   // console.log(pb.authStore.model.id);
 
   try {
-    const record = await pb.collection("chapter").getOne(chapterId || "", {
-      expand: "pages",
-    });
+    const record = await pb.collection('chapter').getOne(chapterId || '', {
+      expand: 'pages'
+    })
 
     // console.warn(record);
     // console.warn(record.expand.pages);
     // debugger;
 
-    return res.status(200).json({ record });
+    return res.status(200).json({ record })
   } catch (err: any) {
-    console.log(err);
+    console.log(err)
 
     return res
       .status(err.response.status || 500)
-      .json({ message: err.response.message, code: err.response.code });
+      .json({ message: err.response.message, code: err.response.code })
   }
 }
--- src/pages/comic-viewer.tsx
+++ src/pages/comic-viewer.tsx
@@ -1,48 +1,48 @@
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google'
 
 // import react dynamic function
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 // Disable server-side rendering for the ComicViewer component by using the dynamic component from Next.js.
 // This will ensure that the component is only rendered on the client-side and not on the server.
-const DynamicComicViewer = dynamic(() => import("react-comic-viewer"), {
-  ssr: false,
-});
+const DynamicComicViewer = dynamic(() => import('react-comic-viewer'), {
+  ssr: false
+})
 
 // const inter = Inter({ subsets: ["latin"] });
 
-const comicChapterId = "juc245vegaemm49";
+const comicChapterId = 'juc245vegaemm49'
 
 export default function Home() {
-  const [chatperPages, setChapterPages] = useState([]);
+  const [chatperPages, setChapterPages] = useState([])
 
   const fetchData = async () => {
-    let pagesSourceList = [];
+    let pagesSourceList = []
     try {
-      const chapterId = new URLSearchParams({ id: comicChapterId });
+      const chapterId = new URLSearchParams({ id: comicChapterId })
 
       const response = await axios.get(
         `/api/chapters/view?${chapterId.toString()}`
-      );
+      )
 
       if (response.data.record.expand.pages)
         pagesSourceList = response.data.record.expand.pages.map(
           (page: any) => page.source
-        );
-      setChapterPages(pagesSourceList);
+        )
+      setChapterPages(pagesSourceList)
     } catch (err: any) {
-      console.warn(err.response.data.code, err.response.data.message);
+      console.warn(err.response.data.code, err.response.data.message)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     // use the new dynamic comic viewer instead of the default.
-    <DynamicComicViewer direction="rtl" pages={chatperPages} />
-  );
+    <DynamicComicViewer direction='rtl' pages={chatperPages} />
+  )
 }
